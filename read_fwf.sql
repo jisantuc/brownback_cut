608b23dcfde6d8a967fa7d536584cc62f98c8c3a
jisantuc <james.santucci@gmail.com>
2014-11-12 23:29:34 -0500

    added some to analysis notebook

--- read_fwf.sql
+++ read_fwf.sql
@@ -1,28 +1,14 @@
-CREATE TABLE census(year char(4),
-/*                    datanum numeric(2),*/
+CREATE TABLE census(year numeric(4),
                     serial char(8),
-/*                    hhwt numeric(10),
-                    gq char(1),
-                    pernum numeric(4),
-                    perwt numeric(10),
-                    hcovany numeric(1),
-                    hcovpriv numeric(1),
-                    hinsemp numeric(1),
-                    hinspur numeric(1),
-                    hinstri numeric(1),
-                    hcovpub numeric(1),
-                    hinscaid numeric(1),
-                    hinscare numeric(1),
-                    hinsva numeric(1),
-                    hinsihs numeric(1),*/
+                    statefip char(2),
                     empstat char(1),
-/*                    empstatd numeric(2),*/
-                    inctot char(7),
+                    inctot numeric(7),
                     migplac1 char(3));
 
-CREATE TABLE staging(data text);
-\copy places_staging FROM /home/james/brownback_cut/usa_00001.dat;
 
-INSERT INTO census(year, serial, empstat, inctot, migplac1)
-    SELECT substring(data,1,4) As year, substring(data,7,8) As serial, substring(data,50,1) As empstat, substring(data,53,7) As inctot, substring(data,60,3) As migplac1
+CREATE TABLE staging(data text);
+\copy staging FROM /home/james/brownback_cut/usa_00003.dat;
+
+INSERT INTO census(year, serial, statefip, empstat, inctot, migplac1)
+    SELECT CAST(substring(data,1,4) AS numeric) AS year, substring(data,7,8) AS serial, substring(data,25,2) AS statefip, substring(data,42,1) AS empstat, CAST(substring(data,45,7) AS numeric) AS inctot, substring(data,52,3) AS migplac1
     FROM staging;
